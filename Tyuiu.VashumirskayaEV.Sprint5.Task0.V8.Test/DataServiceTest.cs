cc6c5a6e61d8ec2b372bc4d06d6bbfe49e92f626
Елизавета Вашумирская <Tyuiu.vashumirskayaEV@outlook.com>
2025-11-29 00:32:20 +0500

    Add project files.

---
+++ Tyuiu.VashumirskayaEV.Sprint5.Task0.V8.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Tyuiu.VashumirskayaEV.Sprint5.Task0.V8.Lib;
+
+namespace Tyuiu.VashumirskayaEV.Sprint5.Task0.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\huawei\source\repos\Tyuiu.VashumirskayaEV.Sprint5\Tyuiu.VashumirskayaEV.Sprint5.Task0.V8\bin\Debug\net8.0\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.VashumirskayaEV.Sprint5.Task0.V8/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.VashumirskayaEV.Sprint5.Task0.V8.Lib;
+
+namespace Tyuiu.VashumirskayaEV.Sprint5.Task0.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Вашумирская Е. В. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Работа с файлами                                                  *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнила: Вашумирская Елизавета Владимировна | СМАРТб-25-1             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить y = (x^3 - 1) / (4x^2) при x = 3, округлить до 3 знаков и     *");
+            Console.WriteLine("* сохранить результат в файл OutPutFileTask0.txt.                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"x = {x}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ :                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: "  + res);
+            Console.WriteLine("Создан!");
+
+            Console.ReadKey();
+        }
+    }
+}

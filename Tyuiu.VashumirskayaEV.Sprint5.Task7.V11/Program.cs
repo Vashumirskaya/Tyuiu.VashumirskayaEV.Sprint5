c971050d66c52978610d4b53e75e313784c20de6
Елизавета Вашумирская <Tyuiu.vashumirskayaEV@outlook.com>
2025-12-13 01:06:08 +0500

    Task7

--- Tyuiu.VashumirskayaEV.Sprint5.Task7.V11/Program.cs
+++ Tyuiu.VashumirskayaEV.Sprint5.Task7.V11/Program.cs
@@ -15,13 +15,13 @@ namespace Tyuiu.VashumirskayaEV.Sprint5.Task7.V11
             Console.WriteLine("* Спринт #5                                                              *");
             Console.WriteLine("* Тема: Работа с файлами                                                 *");
             Console.WriteLine("* Задание #11                                                            *");
-            Console.WriteLine("* Выполнила: Вашумирская Елизавета Владимировна | СМАРТ-25-1              *");
+            Console.WriteLine("* Выполнила: Вашумирская Елизавета Владимировна | СМАРТ-25-1             *");
             Console.WriteLine("**************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                               *");
-            Console.WriteLine("* Из файла удалить все пробелы и строчные русские буквы.                  *");
-            Console.WriteLine("* Результат сохранить в файл OutPutDataFileTask7V11.txt.                  *");
+            Console.WriteLine("* Из файла удалить все пробелы и строчные русские буквы.                 *");
+            Console.WriteLine("* Результат сохранить в файл OutPutDataFileTask7V11.txt.                 *");
             Console.WriteLine("**************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
             Console.WriteLine($"* Исходный файл: {path}");
             Console.WriteLine("**************************************************************************");
 
